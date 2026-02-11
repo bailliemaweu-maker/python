31e102ab34df52241e85f9f2fdea5d55cdfbaaca
bailliemaweu-maker <bailliemaweu@gmail.com>
2026-02-11 15:08:12 +0300

    assign

---
+++ lesson6a.py
@@ -0,0 +1,36 @@
+# 1. function to add two numbers with parameters 
+def addition (num1, num2) :
+    answer = num1 + num2
+    print("The sum is", answer)
+
+# call the function 
+addition (17, 3)
+
+# 2. Function to subtract 
+def subtraction () :
+    num1 = 17
+    num2 = 7
+    answer = num1 - num2 
+    print("The subract is", answer)
+
+# call the function 
+subtraction ()
+
+# 3. function ro multiply 
+
+def multiplication (num1, num2) :
+    answer = num1 * num2
+    print ("THE ANSWER IS", answer)
+
+# call the function 
+multiplication (21,7)
+
+# 4. Function to divide 
+def division () :
+    num1 = 24
+    num2 = 3
+    answer = num1 / num2
+    print ("the answer is", answer)
+
+# call the function 
+division ()
